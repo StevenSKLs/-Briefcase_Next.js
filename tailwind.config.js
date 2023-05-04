16d2f9eb5a71740cb33e2c43bfeac774ef5b74ef
StevenSKLs <davinchisonico@gmail.com>
2023-05-04 15:46:42 -0500

    docs: first personal work with typescript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
   ],
+  // Ensure these match with .storybook/preview.js
+
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        white: '#FFFFFF',
+        blueSky: '#E6ECF5',
+        red: '#EF3061',
+        blueDark: '#07469C',
+        dark: '#333333',
+        blueLight: '#0099E1',
+        grayLight: '#757575',
+        whiteDark: '#F2F2F2',
+      },
+      screens: {
+        xs: '375px',
+        sm: '600px',
+        md: '900px',
+        lg: '1200px',
+        xl: '1536px',
+      },
+      fontFamily: {
+        nunito: ['Nunito'],
+        mulish: ['Mulish'],
       },
     },
+    plugins: [],
   },
-  plugins: [],
-}
+});
