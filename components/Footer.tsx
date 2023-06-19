9424828ed42147ac7a9da94ffc5bc7ea1da1833c
StevenSKLs <davinchisonico@gmail.com>
2023-06-19 01:36:17 -0500

    docs: update_footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ const Footer = () => {
     return (
         <div className=" w-full h-[10%] bg-blue-gray-600 flex justify-between absolute bottom-0 items-center ">
             <p className="text-[3vh]">
-                Estamos trabajando / Ultima actualizacion el 18/06/2023 Hora Mexico 2am
+                Estamos trabajando / Ultima actualizacion el 19/06/2023 Hora Mexico 1am
             </p>
-            <div className="relative w-[17%] h-full ">
+            <div className="relative w-[16%] h-full ">
                 <Image src={'/others/cat_word.gif'} alt="cat" fill />
             </div>
 
